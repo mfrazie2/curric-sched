cd8c0f76db5f4acbd98c911d0d403f4460cd9331
Mike Frazier <michael.s.frazier@gmail.com>
2016-02-15 21:59:07 -0600

    Added hardcoded filtering for days of the week. Will refactor to let user choose.

--- public/app/app.js
+++ public/app/app.js
@@ -58,36 +58,36 @@ angular.module('curric-sched', [])
         }
       ]
     }, 
-    // {
-    //   day: 3, 
-    //   lessons: [
-    //     {
-    //       tech: 'Heroku',
-    //       project: 'Shortly...Again',
-    //       description: 'If you need me, I\'ll be down here on the floor. Dying. I\'ve been using the toilet like an idiot.'
-    //     },
-    //     {
-    //       tech: 'Heroku',
-    //       project: 'Shortly...Again',
-    //       description: 'Dear God, this is Tina from school. Hey Jimmy Jr. Did you see those two squirrels fighting in the courtyard? I can\'t tell where your back ends and your butt begins.'
-    //     }
-    //   ]
-    // },
-    // {
-    //   day: 5, 
-    //   lessons: [
-    //     {
-    //       tech: 'Authentication',
-    //       project: 'Shortly...Last time I swear',
-    //       description: 'You\'re right. I\'m a firestarter and a jinx. I\'m going to destroy this whole family. Just when I think I\'m out, those cheeks pull me right back in. There is one place I like to go.'
-    //     },
-    //     {
-    //       tech: 'Authentication',
-    //       project: 'Shortly...Last time I swear',
-    //       description: 'The boys\' locker room. You can only see up to their ankles. Not if you\'re a fish. Time for the charm bomb to explode. Explore the morgue?'
-    //     }
-    //   ]
-    // }
+    {
+      day: 3, 
+      lessons: [
+        {
+          tech: 'Heroku',
+          project: 'Shortly...Again',
+          description: 'If you need me, I\'ll be down here on the floor. Dying. I\'ve been using the toilet like an idiot.'
+        },
+        {
+          tech: 'Heroku',
+          project: 'Shortly...Again',
+          description: 'Dear God, this is Tina from school. Hey Jimmy Jr. Did you see those two squirrels fighting in the courtyard? I can\'t tell where your back ends and your butt begins.'
+        }
+      ]
+    },
+    {
+      day: 5, 
+      lessons: [
+        {
+          tech: 'Authentication',
+          project: 'Shortly...Last time I swear',
+          description: 'You\'re right. I\'m a firestarter and a jinx. I\'m going to destroy this whole family. Just when I think I\'m out, those cheeks pull me right back in. There is one place I like to go.'
+        },
+        {
+          tech: 'Authentication',
+          project: 'Shortly...Last time I swear',
+          description: 'The boys\' locker room. You can only see up to their ankles. Not if you\'re a fish. Time for the charm bomb to explode. Explore the morgue?'
+        }
+      ]
+    }
     ] 
   };
 }])
